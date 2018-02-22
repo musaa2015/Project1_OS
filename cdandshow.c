0c338d255f4250a0f41afaa33e31840eeb661ec9
Anwar Musa <musaa2015@gmail.com>
2018-02-22 16:49:46 -0500

    Fixed os_chdir wrapper and used it in cdandshow program which changes the CWD fro the program and prints it out if successful

---
+++ cdandshow.c
@@ -0,0 +1,32 @@
+/*
+
+cdandshow.c
+
+  int mainprog(void);
+  int mainprog(int argc);
+  int mainprog(int argc, char **argv);
+  int mainprog(int argc, char **argv, char **envp);
+
+Or as one command (not equivalent):
+
+	gcc -m32 -Wall -nostdlib -o cdandshow cdandshow.c startup32.S os_syscalls.c my_lib.c
+
+*/
+
+#include "os_syscalls.h"
+#include "my_lib.h"
+
+int mainprog(int argc, char **argv)
+{
+	char *pwd[1] = {"/bin/pwd"};
+	int r;	
+	
+	if((r = os_chdir(*(argv+1))) == 0){
+		os_write(1,"Success! New CWD: ",18);
+		os_execve(*pwd, pwd, '\0');
+	}
+	else{
+		os_write(1,"Error: could not cd \n",21);
+	}
+	return 0;
+}
